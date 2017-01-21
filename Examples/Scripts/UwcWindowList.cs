400bd767292e89b25a44efd589e8d3578336a9cd
hecomi <info@hecomi.com>
2017-01-21 23:16:36 +0900

    improve performance especially by threaded updating title.

---
+++ Examples/Scripts/UwcWindowList.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace uWindowCapture
+{
+
+public class UwcWindowList : MonoBehaviour 
+{
+    [SerializeField] GameObject windowListItem;
+    [SerializeField] Transform listRoot;
+    Dictionary<System.IntPtr, UwcWindowListItem> items_ = new Dictionary<System.IntPtr, UwcWindowListItem>();
+
+    void Start()
+    {
+        UwcManager.onWindowAdded.AddListener(OnWindowAdded);
+        UwcManager.onWindowRemoved.AddListener(OnWindowRemoved);
+
+        foreach (var pair in UwcManager.windows) {
+            OnWindowAdded(pair.Value);
+        }
+    }
+
+    void OnWindowAdded(Window window)
+    {
+        if (!window.isAltTabWindow) return;
+
+        var gameObject = Instantiate(windowListItem, listRoot, false);
+        var listItem = gameObject.GetComponent<UwcWindowListItem>();
+        listItem.window = window;
+        items_.Add(window.handle, listItem);
+
+        window.RequestCapture(CapturePriority.Low);
+    }
+
+    void OnWindowRemoved(System.IntPtr handle)
+    {
+        UwcWindowListItem listItem;
+        items_.TryGetValue(handle, out listItem);
+        if (listItem) {
+            Destroy(listItem.gameObject);
+        }
+    }
+}
+
+}
---
+++ Examples/Scripts/UwcWindowListItem.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace uWindowCapture
+{
+
+public class UwcWindowListItem : MonoBehaviour 
+{
+    public Window window { get; set; }
+    
+    [SerializeField] RawImage icon;
+    [SerializeField] Text title;
+    [SerializeField] Text x;
+    [SerializeField] Text y;
+    [SerializeField] Text z;
+    [SerializeField] Text width;
+    [SerializeField] Text height;
+    [SerializeField] Text status;
+
+    void Update()
+    {
+        if (window == null) return;
+
+        icon.texture = window.iconTexture;
+
+        var windowTitle = window.title;
+        title.text = string.IsNullOrEmpty(windowTitle) ? "-No Name-" : windowTitle;
+
+        x.text = window.x.ToString();
+        y.text = window.y.ToString();
+        z.text = window.zOrder.ToString();
+
+        width.text = window.width.ToString();
+        height.text = window.height.ToString();
+
+        status.text = 
+            window.isIconic ? "Iconic" :
+            window.isZoomed ? "Zoomed" :
+            "-";
+    }
+}
+
+}
