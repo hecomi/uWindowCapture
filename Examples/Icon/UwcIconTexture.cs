b47f1a1880509a2012031e38dc17addad075a2d8
hecomi <info@hecomi.com>
2018-06-05 23:01:41 +0900

    separate icon sample to different scene.

---
+++ Examples/Icon/UwcIconTexture.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace uWindowCapture
+{
+
+[RequireComponent(typeof(Renderer))]
+public class UwcIconTexture : MonoBehaviour
+{
+    [SerializeField] UwcWindowObject windowObject;
+    [SerializeField] bool followWindow = true;
+
+    Renderer renderer_;
+    Material material_;
+
+    void Start()
+    {
+        renderer_ = GetComponent<Renderer>();
+        material_ = renderer_.material;
+    }
+
+    void Update()
+    {
+        if (windowObject != null && windowObject.window != null) {
+            UpdateIconTexture();
+            UpdateTransform();
+        }
+    }
+
+    void UpdateIconTexture()
+    {
+        windowObject.window.RequestCaptureIcon();
+        material_.mainTexture = windowObject.window.iconTexture;
+    }
+
+    void UpdateTransform()
+    {
+        var windowPos = windowObject.transform.position;
+        var windowScale = windowObject.transform.localScale; 
+        var iconScale = transform.localScale;
+        windowScale.z = 0;
+        transform.position = 
+            windowPos + 
+            new Vector3((-windowScale.x + iconScale.x) * 0.5f, (windowScale.y + iconScale.y) * 0.5f, 0f);
+    }
+}
+
+}
