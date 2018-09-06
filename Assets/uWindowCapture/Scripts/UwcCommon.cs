1b57c8e6105bbc0ce1382c9614170256123349f6
hecomi <info@hecomi.com>
2018-09-07 01:14:42 +0900

    add UwcWindowTexture.RayCast() and the example.

--- Assets/uWindowCapture/Scripts/UwcCommon.cs
+++ Assets/uWindowCapture/Scripts/UwcCommon.cs
@@ -4,6 +4,28 @@ using UnityEngine.Events;
 namespace uWindowCapture
 {
 
+public enum WindowTextureType
+{
+    Window = 0,
+    Desktop = 1,
+    Child = 2,
+}
+
+public enum WindowTextureCaptureTiming
+{
+    EveryFrame = 0,
+    OnlyWhenVisible = 1,
+    Manual = 2,
+}
+
+public enum WindowTextureScaleControlType
+{
+    BaseScale = 0,
+    FixedWidth = 1,
+    FixedHeight = 2,
+    Manual = 3,
+}
+
 public class UwcEvent : UnityEvent
 {
 }
@@ -18,6 +40,16 @@ public class UwcWindowChangeEvent : UnityEvent<UwcWindow, UwcWindow>
 
 public class UwcWindowTextureEvent : UnityEvent<UwcWindowTexture>
 {
+}
+
+public struct RayCastResult
+{
+    public bool hit;
+    public UwcWindowTexture texture;
+    public Vector3 position;
+    public Vector3 normal;
+    public Vector2 windowCoord;
+    public Vector2 desktopCoord;
 }
 
 public static class UwcSetting
