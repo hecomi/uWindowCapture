458b6a6f813902ba8ec4c49e7aa21922a13375c0
hecomi <info@hecomi.com>
2019-02-02 16:25:07 +0900

    make UwcManager.debugMode static not to generate gameobject just by accessing it #9.

---
+++ Assets/uWindowCapture/Editor/UwcManagerEditor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace uWindowCapture
+{
+
+[CustomEditor(typeof(UwcManager))]
+public class UwcManagerEditor : Editor
+{
+    UwcManager manager
+    {
+        get { return target as UwcManager; }
+    }
+
+    SerializedProperty windowTitlesUpdateTiming;
+
+    void OnEnable()
+    {
+        windowTitlesUpdateTiming = serializedObject.FindProperty("windowTitlesUpdateTiming");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        Draw();
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    void Draw()
+    {
+        var debugMode = (DebugMode)EditorGUILayout.EnumPopup("Debug Mode", manager.debugModeFromInspector);
+        if (debugMode != manager.debugModeFromInspector)
+        {
+            manager.debugModeFromInspector = debugMode;
+        }
+
+        EditorGUILayout.PropertyField(windowTitlesUpdateTiming);
+    }
+}
+
+}
